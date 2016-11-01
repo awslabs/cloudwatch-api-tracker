b2c835cf16a74d03c5cb44ff20e15feed536d161
Jeremy Wallace <jerwallace@users.noreply.github.com>
2016-10-31 21:15:51 -0700

    Add license to app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -5,7 +5,20 @@
  * and publishes metrics in specified batches.
  * VERSION: 0.1.0
  * AUTHORS: Joe Hsieh, Ho Ming Li, Jeremy Wallace, Feng Zhou
- * LICENSE: MIT
+
+     Copyright 2015-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+     Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+         http://aws.amazon.com/apache2.0/
+
+     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+     Note: Other license terms may apply to certain, identified software files contained within or
+     distributed with the accompanying software if such terms are included in the directory containing
+     the accompanying software. Such other license terms will then apply in lieu of the terms of the
+     software license above.
+
  **/
 
 var AWS = require('aws-sdk');
